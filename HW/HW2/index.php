6f52313ea28e2bfab6b68a6cd1c5ce361e5483e9
Rick Rivera <rrivera@csumb.edu>
2018-10-01 00:38:33 +0000

    add HW2 files

--- HW/HW2/index.php
+++ HW/HW2/index.php
@@ -1,42 +1,12 @@
-<?php
-$diceNum = range(1, 6);
-$totalPoints = 0;
 
-shuffle($diceNum);
-
-
-function roll(){
-    global $diceNum, $totalPoints;
-    $rollVal = 0;
-    
-    for($i=0; $i<5; $i++){
-        $dice = $diceNum[rand(0,5)];
-        
-        
-        echo "<img src='img/$dice.png' alt='$dice' title='$dice' />";
-        
-        $rollVal += $dice;
-    }
-    
-    echo "<h3>Points: $rollVal</h3>";
-    $totalPoints += $rollVal;
-    return $rollVal;
-}
-
-function winner($p1, $p2){
-    global $totalPoints;
-    if($p1 > $p2){
-        echo "<br/><br/>You win $totalPoints Points!";
-    }elseif($p1 < $p2){
-        echo "<br/><br/>PC wins $totalPoints Points!";
-    }else{
-        echo '<br/><br/>Tie!';
-    }
-}
-?>
 
 
 <!DOCTYPE html>
+
+<?php
+include 'diceGame.php';
+?>
+
 <html>
     <head>
         <meta charset="UTF-8" >
