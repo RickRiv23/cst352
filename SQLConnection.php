b46d908d7eff71916511c40a1a220773a814c08b
Rick Rivera <rrivera@csumb.edu>
2018-10-22 21:28:27 +0000

    add SQL

--- SQLConnection.php
+++ SQLConnection.php
@@ -2,12 +2,17 @@
 
 function getConnection($dbname){
     //when connecting from Heroku
+    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+    //     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    //     $host = $url["us-cdbr-iron-east-01.cleardb.net"];
+    //     $dbname = substr($url["heroku_e7a1e58035b76c5"], 1);
+    //     $username = $url["bfc457d1a0cdba"];
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["us-cdbr-iron-east-01.cleardb.net"];
-        $dbname = substr($url["heroku_e7a1e58035b76c5"], 1);
-        $username = $url["bfc457d1a0cdba"];
-        $password = $url["pass"];
+        $host = "us-cdbr-iron-east-01.cleardb.net";
+        $dbname = "heroku_e7a1e58035b76c5";
+        $username = "RickRiv";
+        $password = "262931Rjd";
     }else{
         //  Creating database connection
         $host = "localhost";    //  for c9
