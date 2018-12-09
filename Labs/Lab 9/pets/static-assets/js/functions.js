e898cdcf29baa3dab11b8be2d4a4d7b2414f315c
Rick Rivera <rrivera@csumb.edu>
2018-12-09 08:14:26 +0000

    lab 9

--- Labs/Lab 9/pets/static-assets/js/functions.js
+++ Labs/Lab 9/pets/static-assets/js/functions.js
@@ -1,7 +1,29 @@
-// $(document).ready(function(){
-//     $('.petLink').click(function(){
-        
-//         alert('Clicked');
-        
-//     });
-// });
+$(document).ready(function(){
+            $('.pet').click(function(){
+                //alert( $(this).attr("id") );
+                
+                $('#petInfoModal').modal("show");
+                
+                $.ajax({
+                    
+                    type: "GET",
+                    url: "api/getPetInfo.php",
+                    dataType: "json",
+                    data: { "id":$(this).find('.petLink').attr("id") },
+                    success: function(data,status) {
+                    // alert(data.description);
+                    $("#petName").html(data.name);
+                    $("#petDescription").html(data.description);
+                    $("#petImg").attr("src", "img/" + data.pictureURL);
+                    $("#type").html(data.type);
+                    $("#color").html(data.color);
+                    $("#breed").html(data.breed);
+                    
+                    },
+                    complete: function(data,status) { //optional, used for debugging purposes
+                    //alert(status);
+                    }
+                
+                });//ajax
+            }); 
+        });
